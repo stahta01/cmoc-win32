30262939a0ab3d14719d66ae9ab4b9384447efd3
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-08-27 06:47:05 +0000

    moved alib to lib6809

---
+++ src/lib/lib6809/memmove.c
@@ -0,0 +1,17 @@
+
+#define _NAME MEMMOVE
+#define _FILE "alib/memmove.as"
+
+#include "alib.h"
+
+unsigned asm memmove(unsigned dst, unsigned src, unsigned len)
+{
+    asm {
+        ldy     2,s
+        ldx     4,s
+        ldd     6,s
+        lbsr    _NAME
+        ldd     2,s
+    }
+}
+
