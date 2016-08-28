b8ade2b5b4420837012ddd9da6ad8a61b26c03eb
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-08-28 09:36:53 +0000

    Moved conio to libconio

--- examples/fonttest.c
+++ examples/fonttest.c
@@ -7,24 +7,101 @@
 #include <conio.h>
 #include <basic.h>
 
+unsigned s;
+
+unsigned length;
+
+void asm fast(void)
+{
+    asm {
+        pshs    dp,u
+        sts     s
+        ldu     #$400 // dst
+        lds     #$420 // src
+        ldd     #8  // length
+        std     length
+
+        leau 5,u
+
+        copy_blocks:
+        subd #5
+        lbmi copy_bytes
+        puls x,y,dp
+        pshu x,y,dp
+        leau 10,u
+        bra copy_blocks
+
+        copy_bytes:
+        /*
+        leau -3,u
+        addd #7
+        bmi copy_exit
+        puls dp
+        pshu dp
+        */
+        /*
+        leau 2,u
+        subd #1
+        bmi copy_exit
+        puls dp
+        pshu dp
+        leau 2,u
+        subd #1
+        bmi copy_exit
+        puls dp
+        pshu dp
+        leau 2,u
+        subd #1
+        bmi copy_exit
+        puls dp
+        pshu dp
+        leau 2,u
+        subd #1
+        bmi copy_exit
+        puls dp
+        pshu dp
+        leau 2,u
+        subd #1
+        bmi copy_exit
+        puls dp
+        pshu dp
+        leau 2,u
+        subd #1
+        bmi copy_exit
+        puls dp
+        pshu dp
+        leau 2,u
+        */
+        copy_exit:
+        lds s
+        puls dp,u
+    }
+}
+
 int main(void)
 {
-    bgcolor(0);
-    textmode(MODE_L0_32X12);
+    //clrscr();
+    //puts("");
+    //puts("ABCDEFGFabcdefg");
+    //fast();
+    //return 0;
+    //bgcolor(0);
+    textmode(MODE_H0_64X24);
+    //_pmode = 3;
     CLIP(0, 0, 256, 192);
     char s[100];
     int i;
     for (;;) {
         for (i = 0; i < 10; i++) {
             gotox((unsigned char)i);
-            sprintf(s, "Hello There %d,%d\n", wherex(), wherey());
+            sprintf(s, "HELLO THERE %d,%d\n", wherex(), wherey());
             cputs(s);
         }
         s[0] = 63;
         cgets(s);
         cputc('\n');
         printf("You entered %s\n", s + 2);
-        CIRCLE(150, 100, 50, 0);
+        //CIRCLE(150, 100, 50, 1);
     }
     return 0;
 }
