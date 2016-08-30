d2d3dd4f5d18a90fb90f4d74d6d757eabb34880b
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-08-30 13:00:54 +0000

    Added mlock & munlock for 64k memory access

---
+++ examples/memory64k.c
@@ -0,0 +1,31 @@
+
+#pragma target dragon
+
+#include <sys/mman.h>
+#include <string.h>
+#include <conio.h>
+
+int main(void)
+{
+    int i;
+    char s[256];
+    for (i = 0; i < 256; i++) {
+        s[i] = (char)i;
+    }
+    cputs("\n\nCOPY STRING TO $8000\n");
+    if (!mlock(0x8000, 256)) {
+        memcpy(0x8000, s, 256);
+        munlock(0x8000, 256);
+    }
+    cputs("PRESS ANY KEY\n\n");
+    getch();
+
+    cputs("COPY $8000 TO $400\n");
+    if (!mlock(0x8000, 256)) {
+        memcpy(0x400, 0x8000, 256);
+        munlock(0x8000, 256);
+    }
+
+    return 0;
+}
+
---
+++ src/lib/libunistd/mlock.c
@@ -0,0 +1,33 @@
+
+#include <sys/mman.h>
+
+unsigned _mlock_count = 0;
+
+int mlock(void* addr, size_t len)
+{
+    if ((addr + len) >= 0x8000) {
+        if (!_mlock_count) {
+            asm {
+                orcc    #$50    // disable irq and firq
+                sta     $ffdf   // disable rom
+            }
+        }
+        _mlock_count++;
+    }
+    return 0; // SUCCESS = 0
+}
+
+int munlock(void* addr, size_t len)
+{
+    if ((addr + len) >= 0x8000) {
+        _mlock_count--;
+        if (!_mlock_count) {
+            asm {
+                sta     $ffde   // enable rom
+                andcc   #$af    // enable irq and firq
+            }
+        }
+    }
+    return 0; // SUCCESS = 0
+}
+
