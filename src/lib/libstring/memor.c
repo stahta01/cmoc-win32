6a2d953bb9ead09411bbbbe4cf0c6c933cf93ee2
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-09-01 15:34:33 +0000

    Added some mem functions which should be ANSIC

---
+++ src/lib/libstring/memor.c
@@ -0,0 +1,11 @@
+
+#include <string.h>
+
+void* memor(void* ptr, int value, size_t n)
+{
+    for (char* p = (char*)ptr, *e = p + n; p < e; p++) {
+        *p |= (char)value;
+    }
+    return ptr;
+}
+
