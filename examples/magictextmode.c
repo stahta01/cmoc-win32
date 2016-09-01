f13a8098d20a987e434173bc54d8d7ee8208cfcf
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-09-01 05:47:05 +0000

    Improved support for a cool "magic" text mode.

--- examples/magictextmode.c
+++ examples/magictextmode.c
@@ -2,7 +2,7 @@
 // This demo only works for pre-coco2b machines.
 // Go figure!
 
-#pragma options -machine=coco
+#pragma options -machine=cocous
 
 #include <stdio.h>
 #include <conio.h>
@@ -10,19 +10,21 @@
 
 int main(void)
 {
-    char s[100];
+    char s[100], mode = 0;
 
     // We use textmode2, because we need the charset_6x8 (which isn't the default),
     // and this way the default charset's are not linked into the binary.
 
     textmode2(MODE_H0_32X24, charset_6x8);
 
-    // Remove this line to use standard pmode 4
-    *(unsigned char*)0xff22 &= 127;
+    *(unsigned char*)0xff22 ^= 128;
+
+    puts("Four 32x24 CoCo Text Modes");
+    puts("--------------------------");
 
     s[0] = 0;
     while (s[0] != 'x' && s[0] != 'X') {
-        puts("Bitmap Graphics on Text!");
+        puts("Bitmap graphics in text mode!");
         puts("This is a demo of a relatively");
         puts("unknown graphics mode of the");
         puts("CoCo 2. It is limited to blocks");
@@ -33,7 +35,8 @@ int main(void)
         puts("used for text.");
         puts("\nPlease try the editor (x=Exit)");
         gets(s);
-        *(unsigned char*)0xff22 ^= 8;
+        mode++;
+        *(unsigned char*)0xff22 ^= mode & 1 ? 8 : 128;
     }
 
     textmode2(0, 0);
