d1e6c471ba4c4b67e879cd0e1b96a4e016a3c973
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-09-01 04:31:31 +0000

    Fixed a bug in fgets. cgets only supports < 127 string sizes.

--- src/lib/libc/fgets.c
+++ src/lib/libc/fgets.c
@@ -8,7 +8,9 @@ char* fgets(char* s, size_t n, FILE* fp)
     if (fp) {
         return NULL;
     } else {
-        s[0] = (char)n;
+        // Since char is signed, the max for cgets is most
+        // likely 126-ish.
+        s[0] = n < 100 ? (char)n : 100;
         strcpy(s, cgets(s));
         cputc('\n');
         return s;
--- src/lib/libc/gets.c
+++ src/lib/libc/gets.c
@@ -4,10 +4,6 @@
 
 char* gets(char* s)
 {
-    // Note: fgets uses cgets for console input.
-    // which is limited to 255 chars. So, dont use
-    // a max number here that is greater than.
-    // 64 is good I think
-    return fgets(s, 64, stdin);
+    return fgets(s, BUFSIZ, stdin);
 }
 
