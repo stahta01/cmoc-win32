3c9c3ab387fecdbbbbfb6cde50afff8193f0e61a
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-09-03 23:25:44 +0000

    Added Pierre's varptr functions to libvarptr. Added an example (varptr.c)

---
+++ src/lib/libvarptr/setVariableValueToWord.c
@@ -0,0 +1,37 @@
+
+#include "_varptr.h"
+
+//FIXME: doc
+char setVariableValueToWord(char* name, word newAbsValue, byte isNegative)
+{
+    byte* desc = (byte*) findSimpleVariable(name, FALSE);
+    //printf("SET(%s, %u, %u): %p\n", name, newAbsValue, isNegative, desc);
+    if (!desc) {
+        return -1;
+    }
+
+    if (newAbsValue == 0) {
+        desc[0] = 0;
+        * (word*)(desc + 1) = 0;
+        * (word*)(desc + 3) = 0;
+    } else {
+        // Bring highest set bit of newAbsValue to bit 15.
+        byte exponent = 0x80 + 8 * sizeof(word);
+        while ((newAbsValue & 0x8000) == 0) {
+            newAbsValue <<= 1;
+            --exponent;
+        }
+        newAbsValue &= 0x7FFF;              // bit 15 will be the sign bit in the IEEE 754 format
+        if (isNegative) {
+            newAbsValue |= 0x8000;
+        }
+
+        desc[0] = exponent;
+        * (word*)(desc + 1) = newAbsValue;
+        * (word*)(desc + 3) = 0;
+    }
+
+    //printf("%02x %02x %02x %02x %02x\n", desc[0], desc[1], desc[2], desc[3], desc[4]);
+    return 0;
+}
+
