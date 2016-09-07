fd286e86f17eea1a07ff0d9cc5a4002f7e8c4bd8
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-09-07 00:43:02 +0000

    Added CAS:/PTR: file support

--- src/lib/libstdio/fopen.c
+++ src/lib/libstdio/fopen.c
@@ -6,10 +6,15 @@ FILE* fopen(char* name, char* m)
     FILE* fp = NULL;
     if (m[0] == 'r' || m[0] == 'w') {
         char devnum;
-        if (m[1] == 'c') {
+        if (name[0] == 'C' && name[1] == 'O' && name[2] == 'N' && name[3] == ':') {
+            devnum = 0;
+            name += 4;
+        }  else if (name[0] == 'C' && name[1] == 'A' && name[2] == 'S' && name[3] == ':') {
             devnum = -1;
-        } else if (m[1] == 'p') {
+            name += 4;
+        }  else if (name[0] == 'P' && name[1] == 'R' && name[2] == 'T' && name[3] == ':') {
             devnum = -2;
+            name += 4;
         } else {
             for (devnum = _fcbact; devnum > 0; devnum--) {
                 unsigned char* fcb = (unsigned char*)((unsigned*)_FCBV1)[devnum - 1];
