b3c2c110aa90c4f77d81fa9795d5f29047054ea5
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-09-18 18:42:30 +0000

    Added ascii.h. Why this isn't a standard header, I dont know...

---
+++ include/ascii.h
@@ -0,0 +1,43 @@
+
+// http://en.cppreference.com/w/cpp/language/ascii
+
+#ifndef _ASCII_H
+#define _ASCII_H
+
+#define ASCII_NUL   0                           // NULL
+#define ASCII_SOH   1                           // START OF HEADER
+#define ASCII_STX   2                           // START OF TEXT
+#define ASCII_ETX   3                           // END OF TEXT
+#define ASCII_EOT   4                           // END OF TRANSMISSION
+#define ASCII_ENQ   5                           // ENQUIRY
+#define ASCII_ACK   6                           // ACKNOWLEDGE
+#define ASCII_BEL   7                           // BELL
+#define ASCII_BS    8                           // BACKSPACE
+#define ASCII_HT    9                           // HORIZONTAL TAB
+#define ASCII_LF    10                          // LINE FEED
+#define ASCII_VT    11                          // VERTICAL TAB
+#define ASCII_FF    12                          // FORM FEED
+#define ASCII_CR    13                          // CARRIAGE RETURN
+#define ASCII_SO    14                          // SHIFT OUT
+#define ASCII_SI    15                          // SHIFT IN
+#define ASCII_DLE   16                          // DATA LINK ESCAPE
+#define ASCII_DC1   17                          // DEVICE CONTROL 1
+#define ASCII_DC2   18                          // DEVICE CONTROL 2
+#define ASCII_DC3   19                          // DEVICE CONTROL 3
+#define ASCII_DC4   20                          // DEVICE CONTROL 4
+#define ASCII_NAK   21                          // NEGATIVE ACKNOWLEDGE
+#define ASCII_SYN   22                          // SYNCHRONOUS IDLE
+#define ASCII_ETB   23                          // END OF TRANSMISSION BLOCK
+#define ASCII_CAN   24                          // CANCEL
+#define ASCII_EM    25                          // END OF MEDIUM
+#define ASCII_SUB   26                          // SUBSTITUTE
+#define ASCII_ESC   27                          // ESCAPE
+#define ASCII_FS    28                          // FILE SEPARATOR
+#define ASCII_GS    29                          // GROUP SEPARATOR
+#define ASCII_RS    30                          // RECORD SEPARATOR
+#define ASCII_US    31                          // UNIT SEPARATOR
+#define ASCII_SPC   32                          // SPACE
+#define ASCII_DEL   127                         // DELETE
+
+#endif
+
