5fddd29bbfa36fa6cc0e5018a938a76f1d83f974
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-09-25 20:25:14 +0000

    libheap is now water tight. Added examples/heaps.c, which shows how to create 16 heaps in video ram. Any unused memory can now be memory managed. ie: Disk buffers, graphics memory, etc.

--- src/lib/libheap/heap_malloc.c
+++ src/lib/libheap/heap_malloc.c
@@ -1,29 +1,23 @@
 
 #include "_heap.h"
 
-// Note: For some strange reason, this code will not work on Vcc unless we use -O2 or -O0.
-// The code works fine with XRoar, but I don't know about real hardware.
-// To me, it looks like Vcc has a 6809 emulation issue, but I couldn't find it, and I've
-// spent too long looking.
-
 #pragma options --optimize=2
 
 void* heap_malloc(heap_t* heap, int size)
 {
     if (size) {
         size += sizeof(int);
-        for (int last = *heap++; heap < last; heap = (int*)((int)heap + (*heap < 0 ? -*heap : *heap))) {
-            int spot = *heap;
-            if (spot < 0) {
-                spot = 0;
-                for (int* p = heap; p < last && *p < 0; spot += *p, p = (int*)((int)p - *p));
-            } else if (!spot) {
-                spot = (int)heap - last;
+        for (int len, last = *heap++; heap < last; heap = (int*)((int)heap + (len < 0 ? -len : len))) {
+            if ((len = *heap) < 0) {
+                len = 0;
+                for (int* p = heap; p < last && *p < 0; len += *p, p = (int*)((int)p - *p));
+            } else if (!len) {
+                len = (int)heap - last;
             }
-            *heap = spot;
-            if (size <= -spot - 2) {
-                if (size < -spot) {
-                    *(int*)((int)heap + size) = spot + size;
+            *heap = len;
+            if (size <= -len - 2) {
+                if (size < -len) {
+                    *(int*)((int)heap + size) = len + size;
                 }
                 *heap++ = size;
                 return (void*)heap;
