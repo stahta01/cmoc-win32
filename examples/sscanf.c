328f04c5de83cd7572c9ed4217b3b1aea1f2bdc1
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-10-05 03:53:41 +0000

    Added sscanf example.

---
+++ examples/sscanf.c
@@ -0,0 +1,21 @@
+
+// Simple example of using scanf. This code is new, so it is still undergoing bug checking.
+// Please contact me if you find an issue.
+
+#include <conio.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char* src = "    COCO   NUMBER=-1234  COUNT=100 40000ABC (123)";
+    char s[10], a, b, c;
+    int i1, i2, i3;
+    unsigned u;
+    cprintf("COUNT: %d\n",
+            sscanf(src, "%s NUMBER=%i COUNT=%i%u%c%c%c (%d)", s, &i1, &i2, &u, &a, &b, &c, &i3));
+    cprintf("%s,%i,%i,%u,%c,%c,%c,%d\n", s, i1, i2, u, a, b, c, i3);
+
+    return 0;
+}
+
