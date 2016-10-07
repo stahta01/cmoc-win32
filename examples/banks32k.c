0d74f9caa32b8574a91f4f6e5cabfafa6d0ef580
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-10-07 01:48:07 +0000

    Mmm. Maybe now. libbank fixed.

--- examples/banks32k.c
+++ examples/banks32k.c
@@ -1,33 +1,41 @@
 
-#pragma options -machine=cocous
+//#pragma options -machine=cocous
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <bank.h>
 
+#include <machine.h>
+#include <coco/coco3.h>
+
 int main(void)
 {
+    system("WIDTH40");
+
     char* s1 = 0x8000;
-    char* s2 = 0x8100;
+    char* s2 = 0x9000;
 
     // Note: Bank 0 is the 32k under ROM.
     // Bank 15 is the default RAM/ROM mode, but you should always
     // use the return value from bank_set().
     // All banks are 32k mapped to memory $8000
 
-    bank_t bank = bank_set(0);
+#define BANK 0
+
+    bank_t bank = bank_set(BANK);
     strcpy(s1, "THE CMOC STANDARD 32K");
     bank_set(bank);
 
-    bank = bank_set(0);
+    bank = bank_set(BANK);
     strcpy(s2, "MEMORY BANK MODEL");
     bank_set(bank);
 
-    bank = bank_set(0);
+    bank = bank_set(BANK);
     puts(s1);
     bank_set(bank);
 
-    bank = bank_set(0);
+    bank = bank_set(BANK);
     puts(s2);
     bank_set(bank);
 
--- src/lib/bank/bank_set.c
+++ src/lib/bank/bank_set.c
@@ -3,33 +3,38 @@
 
 bank_t _static_bank = 15;
 
+void bank_set_mmu(bank_t bank)
+{
+    byte page = (byte)(bank << 2) & 63;
+    _mmureg[4] = page + 0;
+    _mmureg[5] = page + 1;
+    _mmureg[6] = page + 2;
+    _mmureg[7] = page + 3;
+}
+
 bank_t bank_set(bank_t bank)
 {
-    bank_t result = _static_bank;
-    _static_bank = bank;
     asm {
         seif
     }
-    bool iscoco3 = _machine.typ == MAC_TYP_COCO3;
-    if (iscoco3) {
-        byte page = (byte)(bank << 2) & 63;
-        _mmureg[4] = page + 0;
-        _mmureg[5] = page + 1;
-        _mmureg[6] = page + 2;
-        _mmureg[7] = page + 3;
-    }
-    if (bank == 15) {
-        if (!iscoco3) {
-            _romclr = true;
-        }
+    switch (bank) {
+    case 0:
+        bank_set_mmu(15);
+        _romset = true;
+        break;
+    case 15:
+        bank_set_mmu(15);
+        _romclr = true;
         asm {
             clif
         }
-    } else if (bank == 0) {
-        if (!iscoco3) {
-            _romset = true;
-        }
+        break;
+    default:
+        bank_set_mmu(bank);
+        break;
     }
+    bank_t result = _static_bank;
+    _static_bank = bank;
     return result;
 }
 
