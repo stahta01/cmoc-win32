2faf316bebf778f62bb3c41081cc27fa78509f28
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-10-07 23:49:37 +0000

    A little optimizing for gotoxy (CoCo3)

--- include/coco/super.h
+++ include/coco/super.h
@@ -2,27 +2,38 @@
 #ifndef _COCO_SUPER_H
 #define _COCO_SUPER_H
 
-// ADDITIONAL VARIABLES USED BY SUPER EXTENDED BASIC
+// These locations are copies of _HRMODE & _HRWIDTH.
+// Note: These values overlap CoCo2's _DLBAUD & _TIMOUT, which means they are only valid
+// for CoCo3's. I guess the CoCo3 either moved or removed _DLBAUD/_TIMOUT, therefore
+// _DLBAUD & _TIMOUT are not portable. I'll look into removing them from other headers.
 
-#define _H_CRSLOC   65024                       // FDB  CURRENT LOCATION OF CURSOR
-#define _H_CURSX    65026                       // FCB  X POSITION OF CURSOR
-#define _H_CURSY    65027                       // FCB  Y POSITION OF CURSOR
-#define _H_COLUMN   65028                       // FCB  COLUMNS ON HI-RES ALPHA SCREEN
-#define _H_ROW      65029                       // FCB  ROWS ON HI-RES ALPHA SCREEN
-#define _H_DISPEN   65030                       // FDB  END OF HI-RES DISPLAY SCREEN
-#define _H_CRSATT   65032                       // FCB  CURRENT CURSOR'S ATTRIBUTES
-#define _H_UNUSED   65033                       // FCB  UNUSED
-#define _H_FCOLOR   65034                       // FCB  FOREGROUND COLOR
-#define _H_BCOLOR   65035                       // FCB  BACKGROUND COLOR
-#define _H_ONBRK    65036                       // FDB  ON BRK GOTO LINE NUMBER
-#define _H_ONERR    65038                       // FDB  ON ERR GOTO LINE NUMBER
-#define _H_ERROR    65040                       // FCB  ERROR NUMBER ENCOUNTERED OR $FF (NO ERROR)
-#define _H_ONERRS   65041                       // FDB  ON ERR SOURCE LINE NUMBER
-#define _H_ERLINE   65043                       // FDB  LINE NUMBER WHERE ERROR OCCURRED
-#define _H_ONBRKS   65045                       // FDB  ON BRK SOURCE LINE NUMBER
-#define _H_ERRBRK   65047                       // FCB  STILL UNKNOWN, HAS TO DO WITH ERR, BRK
-#define _H_PCOUNT   65048                       // FCB  PRINT COUNT, CHARACTERS TO BE HPRINTED
-#define _H_PBUF     65049                       // ARR  PRINT BUFFER, HPRINT CHARS. STORED HERE
+#define _H_MODE     230                         //
+#define _H_WIDTH    231                         // 1=WIDTH40, 2=WIDTH80
+
+// Additional variables used by super extended basic
+
+#define _H_CRSLOC   65024                       // WORD  CURRENT LOCATION OF CURSOR
+#define _H_CURSX    65026                       // BYTE  X POSITION OF CURSOR
+#define _H_CURSY    65027                       // BYTE  Y POSITION OF CURSOR
+#define _H_COLUMN   65028                       // BYTE  COLUMNS ON HI-RES ALPHA SCREEN
+#define _H_ROW      65029                       // BYTE  ROWS ON HI-RES ALPHA SCREEN
+#define _H_DISPEN   65030                       // WORD  END OF HI-RES DISPLAY SCREEN
+#define _H_CRSATT   65032                       // BYTE  CURRENT CURSOR'S ATTRIBUTES
+#define _H_UNUSED   65033                       // BYTE  UNUSED
+#define _H_FCOLOR   65034                       // BYTE  FOREGROUND COLOR
+#define _H_BCOLOR   65035                       // BYTE  BACKGROUND COLOR
+#define _H_ONBRK    65036                       // WORD  ON BRK GOTO LINE NUMBER
+#define _H_ONERR    65038                       // WORD  ON ERR GOTO LINE NUMBER
+#define _H_ERROR    65040                       // BYTE  ERROR NUMBER ENCOUNTERED OR $FF (NO ERROR)
+#define _H_ONERRS   65041                       // WORD  ON ERR SOURCE LINE NUMBER
+#define _H_ERLINE   65043                       // WORD  LINE NUMBER WHERE ERROR OCCURRED
+#define _H_ONBRKS   65045                       // WORD  ON BRK SOURCE LINE NUMBER
+#define _H_ERRBRK   65047                       // BYTE  STILL UNKNOWN, HAS TO DO WITH ERR, BRK
+#define _H_PCOUNT   65048                       // BYTE  PRINT COUNT, CHARACTERS TO BE HPRINTED
+#define _H_PBUF     65049                       // BYTS PRINT BUFFER, HPRINT CHARS. STORED HERE
+
+#define _h_mode     (*(byte*)_H_MODE)
+#define _h_width    (*(byte*)_H_WIDTH)
 
 #define _h_crsloc   (*(word*)_H_CRSLOC)
 #define _h_cursx    (*(byte*)_H_CURSX)
--- src/lib/conio/gotoxy.c
+++ src/lib/conio/gotoxy.c
@@ -4,9 +4,11 @@
 void gotoxy(int x, int y)
 {
     if (_is_coco3_mode) {
-        _h_crsloc = 0x2000 + (((_h_column * y) + x) << 1);
         _h_cursx = (byte)x;
         _h_cursy = (byte)y;
+        // A little optimization to multiply y by either 40 or 80.
+        y += (y <<= 2 +_h_width) << 2;
+        _h_crsloc = 0x2000 + ((x + y) << 1);
     } else if (isvidram()) {
         _curpos = _VIDRAM + (y << 5) + x;
     } else if (_conio.settextposition) {
