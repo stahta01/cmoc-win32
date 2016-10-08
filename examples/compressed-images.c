98a78de95d798b45c797a40e1bc84f4babc7318c
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-10-08 03:35:26 +0000

    compressed-images.c example is now working again. We must of saved space with swritef

--- examples/compressed-images.c
+++ examples/compressed-images.c
@@ -1,7 +1,7 @@
 
 // NOTE Due to the bin size, this only works on a no dos extended coco
 
-//#pragma org 7680
+#pragma org 7680
 #pragma options -machine=cocous -nodos
 
 #include <stdlib.h>
@@ -14,17 +14,16 @@
 
 #include "images/1.c"
 #include "images/2.c"
-//#include "images/3.c"
-//#include "images/4.c"
-//#include "images/6.c"
+#include "images/3.c"
+#include "images/4.c"
+#include "images/6.c"
 #include "images/8.c"
 
 void ShowImage(crypt_image_t* image)
 {
-    // vsprintf has grow in size due to 32bit & float support, so
-    // this code was removed to save memory.
-    //systemf("PMODE%d,1", image->type & CRYPT_IMAGE_TYPE_COL ? 3 : 4);
-    //systemf("SCREEN1,%d", image->type & CRYPT_IMAGE_TYPE_CSS ? 1 : 0);
+    systemf("PMODE%d,1:SCREEN1,%d",
+        image->type & CRYPT_IMAGE_TYPE_COL ? 3 : 4,
+        image->type & CRYPT_IMAGE_TYPE_CSS ? 1 : 0);
     crypt_decode(&image->crypt, (void*)_beggrp, 0);
     sleep(4);
 }
@@ -35,9 +34,9 @@ int main(void)
     for (;;) {
         ShowImage((crypt_image_t*)image1);
         ShowImage((crypt_image_t*)image2);
-        //ShowImage((crypt_image_t*)image3);
-        //ShowImage((crypt_image_t*)image4);
-        //ShowImage((crypt_image_t*)image6);
+        ShowImage((crypt_image_t*)image3);
+        ShowImage((crypt_image_t*)image4);
+        ShowImage((crypt_image_t*)image6);
         ShowImage((crypt_image_t*)image8);
     }
     return 0;
