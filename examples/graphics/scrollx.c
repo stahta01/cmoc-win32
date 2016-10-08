8a88785693e479d987bfe491b696cb21330b4542
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-10-08 08:19:45 +0000

    Added a simple scrolling demo

--- examples/graphics/scrollx.c
+++ examples/graphics/scrollx.c
@@ -1,25 +1,26 @@
 
-#pragma options -machine=cocous
+//#pragma options -machine=cocous
 
 #include <equates.h>
 #include <stdlib.h>
 #include <memory.h>
 
-#include "images/image256x192.c"
+#include <crypt/image.h>
+
+#include "images/game128x96.c"
 
 int main(void)
 {
-    system("PMODE4,1:SCREEN1,0");
-    memcpy((void*)_beggrp, image256x192, _endgrp - _beggrp);
+    system("PMODE1,1:SCREEN1,0");
+    crypt_decode(&((crypt_image_t*)image1)->crypt, (void*)_beggrp, 0);
     while (true) {
-        byte* line = (byte*)_beggrp;
-        for (int y = 0; y < 192; y++) {
-            if (y & 32) {
-                memrol(line, 32, line[0] & 128);
-            } else {
-                memror(line, 32, line[31] & 1);
-            }
-            line += 32;
+        byte* line = (byte*)_beggrp + (9 << 5);
+        for (int y = 0; y < 17; y++, line += 32) {
+            memrol(line, 32, line[0] & 128);
+        }
+        line = (byte*)_beggrp + (43 << 5);
+        for (int y = 0; y < 15; y++, line += 32) {
+            memror(line, 32, line[31] & 1);
         }
     }
     return 0;
--- img/build.bat
+++ img/build.bat
@@ -1,28 +1,5 @@
-..\bin\pic2raw -pal=1 -w 256 -h 192 -o image.raw images\1.png
-..\bin\bin2c -v image1 -o 1.c image.raw
 
-..\bin\pic2raw -pal=2 -w 128 -h 192 -o image.raw images\2.png
-..\bin\bin2c -v image2 -o 2.c image.raw
-
-..\bin\pic2raw -pal=3 -w 128 -h 192 -o image.raw images\3.png
-..\bin\bin2c -v image3 -o 3.c image.raw
-
-..\bin\pic2raw -pal=0 -w 256 -h 192 -o image.raw images\4.png
-..\bin\bin2c -v image4 -o 4.c image.raw
-
-..\bin\pic2raw -pal=3 -w 128 -h 192 -o image.raw images\5.png
-..\bin\bin2c -v image5 -o 5.c image.raw
-
-..\bin\pic2raw -pal=0 -w 256 -h 192 -o image.raw images\6.png
-..\bin\bin2c -v image6 -o 6.c image.raw
-
-..\bin\pic2raw -pal=3 -w 128 -h 192 -o image.raw images\7.png
-..\bin\bin2c -v image7 -o 7.c image.raw
-
-..\bin\pic2raw -pal=1 -w 256 -h 192 -o image.raw images\8.png
-..\bin\bin2c -v image8 -o 8.c image.raw
-
-
-del *.raw *.c.i *.asm *.asm.i *.o
+..\bin\pic2raw -pal=2 -o image.raw pmode1-css0.png
+..\bin\bin2c -v image1 -o game128x96.c image.raw
 
 pause
