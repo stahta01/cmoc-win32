5d1c1041f0923eb77be0a3078eeaf3e0138900e9
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-10-09 05:48:51 +0000

    Updated helloworld.c example with a picture of CoCo

--- examples/helloworld.c
+++ examples/helloworld.c
@@ -1,18 +1,15 @@
 
-#pragma target dragon
+// Just a cheery hello from CoCo the clown.
+// Have fun with WinCMOC, and send me any little tid bits you may create.
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-#ifdef __COCO__
-    printf("HELLO COCO WORLD!\n");
-#endif
-#ifdef __DRAGON__
-    printf("HELLO DRAGON WORLD!\n");
-#endif
-    beep(100, 2);
+    system("CLS:LOADM\"CLOWN.PIC:1\"");
+    puts("HELLO EVERYBODY!");
+    beep(40, 1);
     beep(150, 1);
     beep(200, 1);
     return 0;
