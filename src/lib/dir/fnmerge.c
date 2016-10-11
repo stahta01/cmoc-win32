9e0148d2cd308a56faade44c3c53ea208f8a0bc9
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-10-11 11:23:24 +0000

    added asm version of stpcpy. added strmcmp (compare str with mem). Name changes. Sorry. I'll get it right one day.

--- src/lib/dir/fnmerge.c
+++ src/lib/dir/fnmerge.c
@@ -4,25 +4,25 @@
 void fnmerge(char* path, char* drive, char* dir, char* name, char* ext)
 {
     if (drive) {
-        path = _stpcpy(path, drive);
+        path = stpcpy(path, drive);
         if (path[-1] != ':') {
             *path++ = ':';
         }
     }
     if (dir) {
-        path = _stpcpy(path, dir);
+        path = stpcpy(path, dir);
         if (path[-1] != '/') {
             *path++ = '/';
         }
     }
     if (name) {
-        path = _stpcpy(path, name);
+        path = stpcpy(path, name);
     }
     if (ext) {
         if (ext[0] != '.') {
             *path++ = '.';
         }
-        path = _stpcpy(path, ext);
+        path = stpcpy(path, ext);
     }
     *path = 0;
 }
