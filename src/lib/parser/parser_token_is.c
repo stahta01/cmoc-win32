249a7fcc125c566c6cdba1d5b925f8988c35db39
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-10-29 01:18:36 +0000

    Added a few more eat functions. started a simple parser library.

--- src/lib/parser/parser_token_is.c
+++ src/lib/parser/parser_token_is.c
@@ -3,6 +3,6 @@
 
 bool parser_token_is(parser_t* parser, char* str)
 {
-    return strmemcmp(str, parser->tok, parser->pos - parser->tok) == 0;
+    return strmemcmp(str, parser->tok, parser_token_len(parser)) == 0;
 }
 
