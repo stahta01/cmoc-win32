6d66dd9a30414f685f84e8954c037f9dde4d96b5
buzzphp <buzzphp@556934a8-af5c-4d6c-b05a-6ef882a171a6>
2016-11-02 04:41:08 +0000

    Fixed a couple of function names in libsac.

--- src/lib/sac/dlist_pull_tail.c
+++ src/lib/sac/dlist_pull_tail.c
@@ -1,7 +1,7 @@
 
 #include "_sac.h"
 
-dnode_t* list_pull_tail(dlist_t* list)
+dnode_t* dlist_pull_tail(dlist_t* list)
 {
     dnode_t* node = dlist_tail(list);
     return node ? dnode_remove(node) : (dnode_t*)nullptr;
--- src/lib/sac/dlist_push_tail.c
+++ src/lib/sac/dlist_push_tail.c
@@ -1,7 +1,7 @@
 
 #include "_sac.h"
 
-dnode_t* list_push_tail(dlist_t* list, dnode_t* node)
+dnode_t* dlist_push_tail(dlist_t* list, dnode_t* node)
 {
     return dnode_insert(node, list->tail.prev, &list->tail);
 }
